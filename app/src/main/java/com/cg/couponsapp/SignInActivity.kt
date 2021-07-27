d46651073dbb9c5cbbe620ba541a52b53c2df419
vamsi446 <vamsirevuri11@gmail.com>
2021-07-27 09:44:42 +0530

    signin and signup layout

---
+++ app/src/main/java/com/cg/couponsapp/SignInActivity.kt
@@ -0,0 +1,14 @@
+package com.cg.couponsapp
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SignInActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        }
+    }
+
---
+++ app/src/main/java/com/cg/couponsapp/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.cg.couponsapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+    }
+
+}
