7dfaf561c320e913937c286e05f483101fe5eed8
vamsi446 <vamsirevuri11@gmail.com>
2021-07-28 16:50:24 +0530

    smooth bottom bar setup

--- app/src/main/java/com/cg/couponsapp/NavigationActivity.kt
+++ app/src/main/java/com/cg/couponsapp/NavigationActivity.kt
@@ -3,37 +3,51 @@ package com.cg.couponsapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.cg.couponsapp.databinding.ActivityMainBinding
+import com.cg.couponsapp.databinding.ActivityNavigationBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
+import me.ibrahimsn.lib.SmoothBottomBar
+
 
 class NavigationActivity : AppCompatActivity(){
 
     lateinit var mauth: FirebaseAuth
     lateinit var userReference : DatabaseReference
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityNavigationBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navigation)
-
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = findNavController(R.id.bottom_nav_fragment)
         mauth = FirebaseAuth.getInstance()
 
-        val navView : BottomNavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+        setupSmoothBottomMenu()
+    }
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navNews
-            )
-        )
-        setupActionBarWithNavController(navController,appBarConfiguration)
-        navView.setupWithNavController(navController)
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav_menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onStart() {
