7fd23a72cd9a836b602f447bca48818979b4867d
vamsi446 <vamsirevuri11@gmail.com>
2021-07-30 16:27:42 +0530

    added animation to splash screen logo

--- app/src/main/java/com/cg/couponsapp/SplashActivity.kt
+++ app/src/main/java/com/cg/couponsapp/SplashActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
 import com.cg.couponsapp.utils.NetworkUtil
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -14,7 +16,9 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        val logoAnim = AnimationUtils.loadAnimation(this, R.anim.splash_screen)
+        spashLogo.startAnimation(logoAnim)
+        coupon_animation.startAnimation(logoAnim)
         firebaseAuth = FirebaseAuth.getInstance()
 
         Handler().postDelayed({
